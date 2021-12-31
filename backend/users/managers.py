56279d548f2489c2530df583a3a085ab7103f78e
Ivan <BaHbka35@gmail.com>
2021-12-31 15:05:13 +0700

    added tests and user model

---
+++ backend/users/managers.py
@@ -0,0 +1,52 @@
+from django.contrib.auth.base_user import BaseUserManager
+
+
+class UserManager(BaseUserManager):
+    """Custom user manager."""
+
+    def _create_base_user(self, first_name, surname, username,
+                          email, password, **exstra_fields):
+
+        if not first_name:
+            raise ValueError("User must have first name")
+        if not surname:
+            raise ValueError("User must have surname name")
+        if not username:
+            raise ValueError("User must have username name")
+        if not email:
+            raise ValueError("User must have email")
+        if not password:
+            raise ValueError("User must have password name")
+
+        email = self.normalize_email(email)
+        try:
+            user = self.model(first_name=first_name,
+                              surname=surname,
+                              username=username,
+                              email=email,
+                              password=None,
+                              **exstra_fields)
+        except TypeError:
+            raise TypeError("Unexisting field of model")
+
+        user.set_password(password)
+        user.slug = username
+        user.save()
+        return user
+        
+
+    def create_user(self, first_name, surname, username,
+                    email, password, **exstra_fields):
+        exstra_fields["is_superuser"] = False
+        exstra_fields["is_staff"] = False
+        exstra_fields["is_activated"] = False
+        return self._create_base_user(first_name, surname, username,
+                                      email, password, **exstra_fields)
+
+    def create_superuser(self, first_name, surname, username,
+                    email, password, **exstra_fields):
+        exstra_fields["is_superuser"] = True
+        exstra_fields["is_staff"] = True
+        exstra_fields["is_activated"] = True
+        return self._create_base_user(first_name, surname, username,
+                                      email, password, **exstra_fields)
