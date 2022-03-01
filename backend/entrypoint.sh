2b3601a3cfbe07af3912313f34285e85e5b3ca14
Ivan <BaHbka35@gmail.com>
2022-03-01 20:50:48 +0700

    added launch celery from docker-compose

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,4 +1,4 @@
 python manage.py makemigrations --noinput
 python manage.py migrate --noinput
 python manage.py createsuperuser --noinput
-python manage.py runserver 0.0.0.0:8000
+python manage.py runserver 0.0.0.0:8000 & celery -A config worker -l info & celery -A config beat -l info
